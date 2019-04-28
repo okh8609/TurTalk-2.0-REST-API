5c6ef00ef7d0000d7c0747b597a9ec8fbb974f17
KaiHao <okh8609@gmail.com>
2019-04-28 17:04:17 +0800

    登入驗證 及 JWT token完成

--- TT2_API/Controllers/AccountController.cs
+++ TT2_API/Controllers/AccountController.cs
@@ -10,6 +10,13 @@ using System.Text;
 using System.Web;
 using MyForum.Services;
 using System.IO;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using JWT;
+using JWT.Algorithms;
+using JWT.Builder;
+using TT2_API.Services;
+using TT2_API.Filters;
 
 namespace TT2_API.Controllers
 {
@@ -28,7 +35,6 @@ namespace TT2_API.Controllers
     }
 
     //API
-    [Authorize]
     [RoutePrefix("api/account")]
     public class AccountController : ApiController
     {
@@ -38,7 +44,6 @@ namespace TT2_API.Controllers
         // POST api/account/reg
         [HttpPost]
         [Route("reg")]
-        [AllowAnonymous]
         public bool Register([FromBody]AccountRegData regData)
         {
             //宣告新的使用者
@@ -105,12 +110,101 @@ namespace TT2_API.Controllers
         }
 
         // POST api/account/login
+        // 此處已POST撰寫 可改用HTTP基本認證的方式進行帳號密碼的傳遞
         [HttpPost]
         [Route("login")]
-        [AllowAnonymous]
-        public bool Login([FromBody]AccountLoginData data)
+        public HttpResponseMessage Login([FromBody]AccountLoginData data)
         {
-            return true;
+            HttpResponseMessage response = null;
+
+            //檢查帳號與密碼是否正確
+            var r = db.PermanentAccount.Where(a => a.email == data.email).ToList();
+
+            if (r.Count == 0)
+            {
+                // 帳號錯誤
+                response = this.Request.CreateResponse<APIResult>(HttpStatusCode.Unauthorized, new APIResult()
+                {
+                    Success = false,
+                    Message = $"",
+                    Payload = "帳號或密碼不正確"
+                });
+            }
+            else
+            {
+                if (r[0].auth_code != null && r[0].auth_code.Trim() != "")
+                {
+                    // 帳號尚未驗證
+                    response = this.Request.CreateResponse<APIResult>(HttpStatusCode.Unauthorized, new APIResult()
+                    {
+                        Success = false,
+                        Message = $"",
+                        Payload = "帳號尚未通過電子郵件驗證"
+                    });
+                }
+                else if (r[0].password == HashPassword(data.password))
+                {
+                    // 帳號與密碼比對正確，回傳帳密比對正確
+
+                    #region 產生這次通過身分驗證的存取權杖 Access Token
+                    string secretKey = MainHelper.SecretKey;
+
+                    // 設定該存取權杖的有效期限
+                    IDateTimeProvider provider = new UtcDateTimeProvider();
+                    var expDate = provider.GetNow().AddDays(7); //權杖效期7天
+                    var unixEpoch = UnixEpoch.Value; // 1970-01-01 00:00:00 UTC
+                    var secondsSinceEpoch = Math.Round((expDate - unixEpoch).TotalSeconds);
+
+                    //產生Token
+                    var jwtToken = new JwtBuilder()
+                          .WithAlgorithm(new HMACSHA256Algorithm())
+                          .WithSecret(secretKey)
+                          .AddClaim("iss", r[0].uid.ToString()) //
+                          .AddClaim("exp", secondsSinceEpoch)
+                          .AddClaim("role", new string[] { "User", "People" })
+                          .Build();
+                    #endregion
+
+                    response = this.Request.CreateResponse<APIResult>(HttpStatusCode.OK, new APIResult()
+                    {
+                        Success = true,
+                        Message = $"帳號:{r[0].email} / 密碼:{r[0].password}",
+                        Payload = $"{jwtToken}"
+                    });
+                }
+                else
+                {
+                    // 密碼錯誤
+                    response = this.Request.CreateResponse<APIResult>(HttpStatusCode.Unauthorized, new APIResult()
+                    {
+                        Success = false,
+                        Message = $"",
+                        Payload = "帳號或密碼不正確"
+                    });
+                }
+            }
+
+            return response;
+
+        }
+
+
+
+
+        //GET api/account/test
+        [HttpGet]
+        [JwtAuth]
+        [Route("test")]
+        public APIResult Test()
+        {
+            int uid = int.Parse((Request.Properties["user"] as string));
+
+            return new APIResult()
+            {
+                Success = true,
+                Message = $"授權使用者uid為= {uid}",
+                Payload = new string[] { "有提供存取權杖!!", "有提供存取權杖~~" }
+            };
         }
 
 
@@ -137,32 +231,6 @@ namespace TT2_API.Controllers
         }
         #endregion
 
-        // GET api/account/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
 
         protected override void Dispose(bool disposing)
         {
---
+++ TT2_API/Services/MainHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TT2_API.Services
+{
+    public class MainHelper
+    {
+        // 雜湊簽章會用到的金鑰
+        public const string SecretKey = "GXiwDg5MbeJ1XTOYx0NHjPOuuF0QDstcKsVvVBrk";
+    }
+}
